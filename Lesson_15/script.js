ee3602289c47a1af98fe66175ad1e99587764a58
ArtemRysich <110498892+ArtemRysich@users.noreply.github.com>
2024-06-10 22:11:41 +0300

    Lesson_15

---
+++ Lesson_15/script.js
@@ -0,0 +1,183 @@
+// ******* Додавання прослуховувача подій на кожен елемент ******* \\
+// Отримай колір квадратика по якому було здійснено клік
+
+// const container = document.querySelector('.js-container');
+
+// [...container.children].forEach((box) => {
+//   box.addEventListener('click', handlerGetColor)
+// })
+
+// function handlerGetColor(evt){
+//   const color = evt.currentTarget.dataset.color;
+// console.log(color)
+// }
+
+// ************** Делегування подій ************** \\
+// Отримай колір квадратика по якому було здійснено клік
+
+// const container = document.querySelector(".js-container");
+
+// container.addEventListener("click", handlerGetColor);
+
+// function handlerGetColor(evt) {
+//   // console.log('currentTarget', evt.currentTarget)
+//   // console.log('target', evt.target)
+
+//   // if(evt.target === evt.currentTarget){
+//   //   return;
+//   // }
+
+//   if (!evt.target.classList.contains("js-box")) {
+//     return;
+//   }
+
+//   // console.log("target", evt.target);
+//   const color = evt.target.dataset.color;
+//   console.log(color)
+// }
+
+// **************** stopPropagation **************** \\
+
+// const red = document.querySelector(".js-box-red");
+// const green = document.querySelector(".js-box-green");
+// const black = document.querySelector(".js-box-black");
+
+// red.addEventListener('click', handlerClick);
+// green.addEventListener('click', handlerClick);
+// black.addEventListener('click', handlerClick)
+
+// function handlerClick(evt) {
+//   const isConfirm = confirm(`click на елементі ${evt.currentTarget.id}, викликати метод stopPropagation?`)
+
+//   if (isConfirm) {
+//     evt.stopPropagation();
+//   }
+//   // console.log(evt.currentTarget.id)
+// }
+
+// **************** stopImmediatePropagation **************** \\
+// const red = document.querySelector(".js-box-red");
+
+// red.addEventListener("click", firstHandlerClick);
+// red.addEventListener("click", secondHandlerClick);
+// red.addEventListener("click", thirdHandlerClick);
+
+// function firstHandlerClick(e) {
+//   alert("First handler");
+
+//   const isConfirm = confirm("Викликати метод stopImmediatePropagation?");
+
+//   if (isConfirm) {
+//     e.stopImmediatePropagation();
+//   }
+// }
+
+// function secondHandlerClick(e) {
+//   alert("Second handler");
+
+//   const isConfirm = confirm("Викликати метод stopImmediatePropagation?");
+
+//   if (isConfirm) {
+//     e.stopImmediatePropagation();
+//   }
+// }
+
+// function thirdHandlerClick(e) {
+//   alert("Third handler");
+
+//   const isConfirm = confirm("Викликати метод stopImmediatePropagation?");
+
+//   if (isConfirm) {
+//     e.stopImmediatePropagation();
+//   }
+// }
+
+// **************** Практика **************** \\
+// Створи картки з товарами на основі масиву products, приклад картки https://prnt.sc/KmgDlzqOIA3M
+// Реалізуй делегування подій на колекції карток
+// Після кліку на картку повинно з'являтись модальне вікно з детальною інформацією про продукт, приклад модального вікна https://prnt.sc/vWNoCeZcw7ii
+// Для реалізації модального вікна використай бібліотеку basicLightbox (https://github.com/electerious/basicLightbox)
+
+const products = [
+  {
+    id: 1,
+    img: "https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG",
+    name: "Монітор",
+    price: 3000,
+    description: "23-дюймовий монітор з Full HD роздільною здатністю.",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzWqRMI3HQiDfICHAmbArmaP4uOOIjfz0sDITv0dfkpb0mbbgX",
+    name: "Ноутбук",
+    price: 20000,
+    description: "Легкий та потужний ноутбук з 15-дюймовим дисплеєм та SSD.",
+  },
+  {
+    id: 3,
+    img: "https://cdn.27.ua/799/66/39/6841913_1.jpeg",
+    name: "Смартфон",
+    price: 8000,
+    description: "Оснащений потрійною камерою та багатоядерним процесором.",
+  },
+  {
+    id: 4,
+    img: "https://cdn.27.ua/799/b6/16/4371990_1.jpeg",
+    name: "Планшет",
+    price: 12000,
+    description:
+      "10-дюймовий планшет з високою продуктивністю та Retina дисплеєм.",
+  },
+];
+
+const list = document.querySelector(".js-products");
+
+list.insertAdjacentHTML("afterbegin", createMarkup(products));
+list.addEventListener("click", handlerGetProduct);
+
+function handlerGetProduct(evt) {
+  if (evt.currentTarget === evt.target) {
+    return;
+  }
+
+  const parent = evt.target.closest(".js-item");
+  const currentId = Number(parent.dataset.productId);
+  const currentProduct = products.find(({ id }) => id === currentId);
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+      <img src="${currentProduct.img}" alt="${currentProduct.name}">
+      <h2>${currentProduct.name}</h2>
+      <h3>${currentProduct.price}</h3>
+      <p>${currentProduct.description}</p>
+    </div>
+  `);
+
+  instance.show();
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ img, name, price, id }) => `
+      <li class="item js-item" data-product-id="${id}">
+        <img src="${img}" alt="${name}">
+        <h2>${name}</h2>
+        <p>Ціна: ${price} грн</p>
+      </li>
+`
+    )
+    .join("");
+}
+
+// list.append()
+// list.appendChild()
+
+// const fragment = document.createDocumentFragment();
+
+// fragment.appendChild(document.createElement("li"));
+
+
+// console.log(fragment)
+
+// list.appendChild(fragment)
